e050bee367a5c3e4383f336c37e432b109d86d4d
JonCabot <jfc0053@gmail.com>
2022-05-28 07:51:47 -0500

    completed assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
 class Post < ApplicationRecord
+  # basic validations
+  validates :title, presence: true
+  validates :content, length: { minimum: 250 }
+  validates :summary, length: { maximum: 250 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }  #also ['Fiction', 'Non-Fiction']
+
+  # custom validation
+  validate :is_title_clickbaity
+
+  CLICKBAIT_PATTERNS = [
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def is_title_clickbaity
+    if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }  #validator; if none of these patterns are present, return true, otherwise false => error handling.
+      errors.add(:title, "must be clickbait") #add method; attribute + message
+    end
+  end
 end
